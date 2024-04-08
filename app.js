d50d21d03b0fbcf5529c84214595bccc1ee765e4
Arizom <157034570+Ariz0m@users.noreply.github.com>
2024-04-07 22:09:15 -0600

    Decrypt function

--- app.js
+++ app.js
@@ -63,53 +63,93 @@ function encriptador(textoPlano, lista) {
     for (palabras in textoDividido) {
         palabras = parseInt(palabras);
         let palabra = textoDividido[palabras];
-        console.log(`El array a encriptar es: ${palabra}`);
-        for (let i = 0; i < palabra.length; ) {
-            console.log(`Se está recorriendo la palabra ${palabra}. La longitud de la palabra es ${palabra.length}.`);
-            if (buscarCoincidencia(palabra[i], lista)) {
-                console.log(`El valor de i es ${i}.`);
-                let pos = buscarPosicion(palabra[i], lista);
-                if (i === palabra.length - 1) {    
-                    textoDividido[palabras] = i > 0 ? `${palabra.substring(0, i)}${otraLista(lista)[pos]}${palabra.substring(i + 1)}` : `${otraLista(lista)[pos]}${palabra.substring(i + 1)}`;
-                    console.log(`La parte del array ahora es: ${textoDividido[palabras]}`);
-                    palabra = textoDividido[palabras];
-                    break;
+        console.log(`El array a encriptar es: ${palabra} ${typeof(palabra)}. ${textoDividido}`);
+        if (lista === vocales) {
+            for (let i = 0; i < palabra.length; ) {
+                console.log(`Se está recorriendo la palabra ${palabra}. La longitud de la palabra es ${palabra.length}.`);
+                if (buscarCoincidencia(palabra[i], lista)) {
+                    console.log(`El valor de i es ${i}.`);
+                    let pos = buscarPosicion(palabra[i], lista);
+                    if (i === palabra.length - 1) {    
+                        textoDividido[palabras] = i > 0 ? `${palabra.substring(0, i)}${otraLista(lista)[pos]}${palabra.substring(i + 1)}` : `${otraLista(lista)[pos]}${palabra.substring(i + 1)}`;
+                        console.log(`La parte del array ahora es: ${textoDividido[palabras]}`);
+                        palabra = textoDividido[palabras];
+                        break;
+                    }
+                    else {
+                        textoDividido[palabras] = i > 0 ? `${palabra.substring(0, i)}${otraLista(lista)[pos]}${palabra.substring(i + 1)}` : `${otraLista(lista)[pos]}${palabra.substring(i + 1)}`;
+                        console.log(`La parte del array ahora es: ${textoDividido[palabras]}`);
+                        palabra = textoDividido[palabras];
+                        i += otraLista(lista)[pos].length;
+                        console.log(`Se añadió a i ${otraLista(lista)[pos].length}`);
+                    }
+                } else if (!buscarCoincidencia(palabra[i], lista)) {
+                    i++;
+                    console.log(`Debido a que no se encontraron coincidencias, se pasa a la siguiente letra.`);
                 }
-                else {
-                    textoDividido[palabras] = i > 0 ? `${palabra.substring(0, i)}${otraLista(lista)[pos]}${palabra.substring(i + 1)}` : `${otraLista(lista)[pos]}${palabra.substring(i + 1)}`;
-                    console.log(`La parte del array ahora es: ${textoDividido[palabras]}`);
-                    palabra = textoDividido[palabras];
-                    i += otraLista(lista)[pos].length - 1;
-                }
-            } else if (!buscarCoincidencia(palabra[i], lista)) {
-                i++;
-                console.log(`Debido a que no se encontraron coincidencias, se pasa a la siguiente letra.`);
             }
         }
+        else if (lista === encriptadas) {
+            if (buscarCoincidencia(palabra, lista)) {
+                while (buscarCoincidencia(palabra, lista)) {
+                    console.log(textoDividido);
+                    let pos = buscarPosicion(palabra, lista);
+                    textoDividido[palabras] = pos[0] > 0 ? `${palabra.substring(0, pos[0])}${otraLista(lista)[pos[1]]}${palabra.substring(pos[0] + (lista[pos[1]].length))}` : `${otraLista(lista)[pos[1]]}${palabra.substring(pos[0] + lista[pos[1]].length)}`;
+                    palabra = textoDividido[palabras];
+                    console.log(`xdddd ${textoDividido}`);   
+                }
+            } else {palabra++}
+        }
     }
+    console.log(textoDividido);
     console.log(`El texto encriptado es ${textoDividido.join(' ')}.`);
     return textoDividido.join(' ');
 }
 
-//Esto solo es para ver si se pone si recuerdo bien cómo usar los comandos de git.
+function desencriptar(palabraADesencriptar) {
+    
+}
+//Esto solo es para ver si se pone si recuerdo bien cómo usar los comandos de git. It didn't work xd But I'd learned how to do it correctly
 
-function buscarCoincidencia(n, r) {
-    console.log(`Se están buscando las coincidencias de ${n} en ${r}`);
-    for (contenido in r) {
-        contenido = parseInt(contenido);
-        if (r[contenido] === n) { 
-            return true;
-        } else {console.log(`No se encontraron coincidencias.`);}
+function buscarCoincidencia(elemento, lista) {
+    console.log(`Se están buscando las coincidencias de ${elemento} en ${lista}`);
+    if (lista === vocales) {
+        for (contenido in lista) {
+            contenido = parseInt(contenido);
+            if (lista[contenido] === elemento) { 
+                    return true;
+                } else {console.log(`No se encontraron coincidencias.`);}
+        }
+    }
+    else if (lista === encriptadas) {
+        for (contenido in lista) {
+            console.log(`${contenido} ${lista[contenido]}`);
+            if (elemento.includes(lista[contenido])) {
+                return true;
+            } else {console.log(`No se encontraron coincidencias, perro.`);}
+        }
     }
 }
 
-function buscarPosicion(n, r) {
-    console.log(`Se está buscando la posición de ${n} en ${r}`);
-    for (pos in r) {
-        pos = parseInt(pos);
-        if (r[pos] === n) {
-            console.log(`Posición encontrada en ${pos}`);
-            return pos;
+function buscarPosicion(elemento, lista) {
+    console.log(`Se está buscando la posición de ${elemento} en ${lista}`);
+    if (lista === vocales) {
+        for (pos in lista) {
+            pos = parseInt(pos);
+            if (lista[pos] === elemento) {
+                console.log(`Posición encontrada en ${pos}`);
+                return pos;
+            }
+        }
+    }
+    else if (lista === encriptadas) {
+        for (pos in lista) {
+            pos = parseInt(pos);
+            console.log(elemento.indexOf(lista[pos]));
+            if (elemento.indexOf(lista[pos]) !== -1) {
+                console.log(`La condition is true && ${elemento.indexOf(lista[pos])}`);
+                return [elemento.indexOf(lista[pos]), pos];
+            }
         }
     }
 }
